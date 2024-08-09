af82ff8606c22c86e23d89dcb3c532caee7b76bd
Sean Nicholls <281460+0x736E@users.noreply.github.com>
2024-08-09 01:16:16 +0100

    refactored tests

--- src/detector/winnow/aws/api_key_test.rs
+++ src/detector/winnow/aws/api_key_test.rs
@@ -1,39 +1,39 @@
 use crate::detector::winnow::aws;
 use crate::detector::winnow::error::DetectorErrorKind;
 
-#[test]
-fn tp_valid() {
-    let test_case = r#"AKIAXR2OBLUTM8DTZV7F"#;
+pub fn assert_valid_key(test_case: &str) {
     let mut input = test_case;
-
     let result = aws::api_key::parse(&mut input);
     assert_eq!(result.is_err(), false);
     assert_eq!(test_case, result.unwrap().to_owned());
 }
 
-#[test]
-fn fp_invalid_prefix() {
-    let mut input = r#"ABCDXR2OBLUTM8DTZV7F"#;
-
+pub fn assert_invalid_key(test_case: &str, err: DetectorErrorKind) {
+    let mut input = test_case;
     let result  = aws::api_key::parse(&mut input);
     assert_eq!(result.is_err(), true);
-    assert_eq!(result.unwrap_err().kind, DetectorErrorKind::NoMatch);
+    assert_eq!(result.unwrap_err().kind, err);
+}
+
+#[test]
+fn tp_valid() {
+    assert_valid_key(r#"AKIAXR2OBLUTM8DTZV7F"#)
+}
+
+#[test]
+fn fp_invalid_prefix() {
+    assert_invalid_key(r#"ABCDXR2OBLUTM8DTZV7F"#,
+                       DetectorErrorKind::NoMatch);
 }
 
 #[test]
 fn fp_invalid_base64() {
-    let mut input = r#"AKIAXR2O:><TM8DTZV7F"#;
-
-    let result  = aws::api_key::parse(&mut input);
-    assert_eq!(result.is_err(), true);
-    assert_eq!(result.unwrap_err().kind, DetectorErrorKind::NoMatch);
+    assert_invalid_key(r#"AKIAXR2O:><TM8DTZV7F"#,
+                       DetectorErrorKind::NoMatch);
 }
 
 #[test]
 fn fp_invalid_length() {
-    let mut input = r#"AKIAXR2OBLUTM8DTZV7FXR2OBLUTM8DTZV7F"#;
-
-    let result  = aws::api_key::parse(&mut input);
-    assert_eq!(result.is_err(), true);
-    assert_eq!(result.unwrap_err().kind, DetectorErrorKind::InvalidSize);
+    assert_invalid_key(r#"AKIAXR2OBLUTM8DTZV7FXR2OBLUTM8DTZV7F"#,
+                       DetectorErrorKind::InvalidSize);
 }
